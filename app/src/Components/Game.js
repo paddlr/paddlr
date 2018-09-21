a3016f7df82a6a3bfe2afbefa4aa87025ce5632f
dani <daniboo83@gmail.com>
2018-09-21 11:52:20 +0100

    add player 2 points

--- app/src/Components/Game.js
+++ app/src/Components/Game.js
@@ -1,22 +1,25 @@
-import React, { Component } from 'react';
-import Player from './Player';
+import React, { Component } from "react";
+import Player from "./Player";
 
 class Game extends Component {
-
   constructor(props) {
     super(props);
-    this.state = { player1Points: 0, player2Points:0 }
+    this.state = { player1Points: 0, player2Points: 0 };
   }
-  
-  render() {
 
+  render() {
     const { player1Points, player2Points } = this.state;
 
     return (
       <div>
-        <Player points={player1Points} onScoreIncremented={
-          () => this.setState({ player1Points: player1Points+1 })}/>
-        <Player/>
+        <Player
+          points={player1Points}
+          onScoreIncremented={() => this.setState({ player1Points: player1Points + 1 })}
+        />
+        <Player
+          points={player2Points}
+          onScoreIncremented={() => this.setState({ player2Points: player2Points + 1 })}
+        />
       </div>
     );
   }
