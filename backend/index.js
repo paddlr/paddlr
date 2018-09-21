df63179476a2d3d527c0885aef2755f138629ddd
Angela Wolff <warriorwomenblog@gmail.com>
2018-09-21 12:13:19 +0100

    Adding production env instructions for index.js

--- backend/index.js
+++ backend/index.js
@@ -11,23 +11,24 @@ const morgan = require("morgan");
 const path = require('path')
 const app = express();
 
-
 mongoose.connect(process.env.DB,  { useNewUrlParser: true });
-
-// Serve the static files from the React app
-app.use(express.static(path.join(__dirname, '/../../frontend/build')));
-
 mongoose.Promise = global.Promise;
 
+// API calls
 app.use(bodyParser.json());
 app.use('/api/users', userRoutes);
 app.use('/api/games', gameRoutes);
 app.use(morgan("dev"));
 
-// Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
-  res.sendFile(path.join(__dirname+'/../../frontend/build', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  // Serve the static files from the React app
+  app.use(express.static(path.join(__dirname, '/../../frontend/build')));
+
+  // Handles any requests that don't match the ones above
+  app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/../../frontend/build', 'index.html'));
+  });
+}
 
 app.use(function(err, req, res, next){
   // we send the error with an error message
