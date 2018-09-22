419ef93ab6eefd0057c1d637bff4c31b6400967d
Tim Blanchard <tim.blanchard@hotmail.co.uk>
2018-09-22 14:01:52 +0100

    who serves first buttons now work correctly

--- app/src/Components/Game.js
+++ app/src/Components/Game.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Player from "./Player";
-import Paddle from "./Paddle"
+import Paddle from "./Paddle";
 
 class Game extends Component {
   constructor(props) {
@@ -9,7 +9,7 @@ class Game extends Component {
       player1Points: 0,
       player2Points: 0,
       winner: null,
-      toServe: 1
+      toServe: 0
     };
   }
 
@@ -32,14 +32,25 @@ class Game extends Component {
     this.state.toServe === 1 ? this.setState({ toServe: 2 }) : this.setState({ toServe: 1 });
   }
 
-  scoreButtonClick() {
+  scoreButtonClick(player) {
     //handles all the click methods
-    console.log(`the next person to serve is ${this.state.toServe}`);
-
-    this.findNextServe();
-    this.findWinner();
+    if (this.state.toServe === 0) {
+    this.setState({toServe: player })
+    
+    } else {
+      if (player === 1) {
+        this.setState({ player1Points: this.state.player1Points + 1 }, () => {
+          this.findNextServe();
+          this.findWinner();
+        });
+      } else {
+        this.setState({ player2Points: this.state.player2Points + 1 }, () => {
+          this.findNextServe();
+          this.findWinner();
+        });
+      }
+    }
   }
-
   render() {
     const { player1Points, player2Points, toServe } = this.state;
 
@@ -47,29 +58,15 @@ class Game extends Component {
       return (
         <div>
           <div className="left">
-            <Player
-              points={player1Points}
-              onScoreIncremented={() =>
-                this.setState({ player1Points: player1Points + 1 }, () => {
-                  this.scoreButtonClick();
-                })
-              }
-            />
-            {toServe === 1 ? <Paddle direction = 'paddle-pic-left'/> : null}
+            <Player toServe={toServe} points={player1Points} onScoreIncremented={() => this.scoreButtonClick(1)} />
+            {toServe === 1 ? <Paddle direction="paddle-pic-left" /> : null}
           </div>
           <div className="right">
-            <Player
-              points={player2Points}
-              onScoreIncremented={() =>
-                this.setState({ player2Points: player2Points + 1 }, () => {
-                  this.scoreButtonClick();
-                })
-              }
-            />
-            {toServe === 2 ? <Paddle direction = 'paddle-pic-right'/> : null}
+            <Player toServe={toServe} points={player2Points} onScoreIncremented={() => this.scoreButtonClick(2)} />
+
+            {toServe === 2 ? <Paddle direction="paddle-pic-right" /> : null}
           </div>
           <button onClick={() => console.log(this.state)}>show me state</button>
-          <h1>the next person to serve is player {this.state.toServe} </h1>
         </div>
       );
     } else return <div> the winner is player {this.state.winner}</div>;
