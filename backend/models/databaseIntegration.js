38e3b3896d660c76bae9fa8ae07b36f10f340125
Angela Wolff <warriorwomenblog@gmail.com>
2018-09-23 17:02:52 +0100

    Revising the slack integration function

--- backend/models/databaseIntegration.js
+++ backend/models/databaseIntegration.js
@@ -1,31 +1,30 @@
 const Users = require('../models/users');
+const _ = require('lodash');
+
 function Database(){
 
 }
 
-Database.prototype.integration = function(slack_data){
-  var jsonLength = Object.keys(slack_data).length;
-  console.log(Object.keys(slack_data));
-  for (var i = 0; i <= jsonLength; i++) {
-    Users.findOne({slack_id: slack_data.members[i].id})
-      .then((user) => {
-        if (user) {
-          console.log(slack_data.members[i].id, ' already exists');
-        } else {
-            Users.create({
+Database.prototype.integration = async (slack_data) => {
+  let newUsers = [];
+
+  for (let i = 0; i < slack_data.members.length; i++) {
+      const user = await Users.findOne({slack_id: slack_data.members[i].id});
+      if (typeof user === 'undefined') {
+          newUsers.push({
               name: slack_data.members[i].real_name,
               slack_id: slack_data.members[i].id,
               slack_image: slack_data.members[i].profile.image_192,
               slack_image_48: slack_data.members[i].profile.image_48,
               slack_image_512: slack_data.members[i].profile.image_512
-            })
-            console.log(slack_data.members[i].real_name, " created");
-        }
-      })
-      .catch((err) => {
-        console.log(err)
-      });
-    }
+          });
+      };
   }
 
-module.exports = Database;
+  const newSlackUsers = await Users.insertMany(newUsers);
+  console.log('New users created ', newSlackUsers);
+
+}
+
+
+  module.exports = Database;
