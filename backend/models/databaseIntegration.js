57b39ff9d29f04fd38750eddfb0c503f9036b13d
Angela Wolff <warriorwomenblog@gmail.com>
2018-09-23 14:27:04 +0100

    Re-writing the slack integration method

--- backend/models/databaseIntegration.js
+++ backend/models/databaseIntegration.js
@@ -5,22 +5,26 @@ function Database(){
 Database.prototype.integration = function(slack_data){
   var jsonLength = Object.keys(slack_data).length;
   var i;
-
-  for (i = 0; i <= jsonLength; i++) {
-    if (Users.find({slack_id: slack_data.members[i].id}) != 'undefined'){
-      console.log(slack_data.members[i].id);
-    }
-    else{
-      Users.create({
-        name: slack_data.members[i].real_name,
-        slack_id: slack_data.members[i].id,
-        slack_image: slack_data.members[i].profile.image_192,
-        slack_image_48: slack_data.members[i].profile.image_48,
-        slack_image_512: slack_data.members[i].profile.image_512
+  for (i = 1; i = jsonLength; i++) {
+    Users.findOne({slack_id: slack_data.members[i].id})
+      .then((users) => {
+        if (users) {
+          console.log(slack_data.members[i].id, ' already exists');
+        } else {
+            Users.create({
+              name: slack_data.members[i].real_name,
+              slack_id: slack_data.members[i].id,
+              slack_image: slack_data.members[i].profile.image_192,
+              slack_image_48: slack_data.members[i].profile.image_48,
+              slack_image_512: slack_data.members[i].profile.image_512
+            })
+            console.log(slack_data.members[i].real_name, " created");
+        }
       })
-      console.log("All people created");
+      .catch((err) => {
+        console.log(err)
+      });
     }
   }
-}
 
 module.exports = Database;
