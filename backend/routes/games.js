21026134d9304d173cb4f10bfbf7a37c36198950
Angela Wolff <warriorwomenblog@gmail.com>
2018-09-24 17:56:36 +0100

    Practising with player scores in Games POST

--- backend/routes/games.js
+++ backend/routes/games.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Games = require('../models/games');
-
+const Users = require('../models/users');
 
 router.get("/", function(req, res, next){
   Games.find({}).then(function(Games){
@@ -10,13 +10,13 @@ router.get("/", function(req, res, next){
   .catch(next)
 });
 
-router.post("/", function(req, res,next){
-  Games.create(req.body).then(function(Games){
-    res.send(Games);
-  })
-  .catch(next)
+router.post("/", async(req, res) => {
+  const player_1 = await Users.findOne({_id: req.body.players[0].player_id});
+  const player_2 = await Users.findOne({_id: req.body.players[1].player_id});
+  
+  console.log('Player 1: ', player_1.name, 'vs Player 2: ', player_2.name);
+  res.send(Games);
 });
-
 
 router.get("/:id", function(req, res, next){
   Games.findById({_id: req.params.id}).then(function(selectedGame){
@@ -24,7 +24,6 @@ router.get("/:id", function(req, res, next){
   })
   .catch(next)
 });
-
 
 router.delete("/:id", function(req, res, next){
   Games.findOneAndDelete({_id: req.params.id}).then(function(Games){
