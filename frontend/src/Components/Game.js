a62f0158dd32aa88a9a8a268a04591bc2e362bb9
dani <daniboo83@gmail.com>
2018-09-25 12:58:57 +0100

    Tim's serve logic is implemented in the background

--- frontend/src/Components/Game.js
+++ frontend/src/Components/Game.js
@@ -7,6 +7,14 @@ import { setPlayerScore, endGame, sendMatchToLeaderboard } from "../redux/action
 import { getWinningPlayer } from "../helpers/winningPlayer";
 
 class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nextServeCounter: 2,
+      toServe: 1,
+    };
+  }
+
   componentDidMount = () => {
     const { players, history, endGame } = this.props;
     if (!players.every(player => player.id)) {
@@ -21,6 +29,18 @@ class Game extends Component {
       this.props.sendMatchToLeaderboard();
     }
   };
+
+  findNextServe() {
+    if (this.state.nextServeCounter !== 5) {
+      this.setState({ nextServeCounter: this.state.nextServeCounter + 1 });
+    } else {
+      this.setState({ nextServeCounter: 1 }, () => this.swapServes());
+    }
+  }
+
+  swapServes() {
+    this.state.toServe === 1 ? this.setState({ toServe: 2 }) : this.setState({ toServe: 1 });
+  }
 
   render() {
     const { winningPlayer, players, setPlayerScore } = this.props;
@@ -39,7 +59,15 @@ class Game extends Component {
                 "WINNER"
               ) : (
                 <div>
-                  <button onClick={() => setPlayerScore(index, 1)}>+</button>
+                  <button
+                    onClick={() => {
+                      setPlayerScore(index, 1);
+                      this.findNextServe();
+                      console.log(`the next person to serve is ${this.state.toServe}`);
+                    }}
+                  >
+                    +
+                  </button>
                   <button disabled={player.score === 0} onClick={() => setPlayerScore(index, -1)}>
                     -
                   </button>
