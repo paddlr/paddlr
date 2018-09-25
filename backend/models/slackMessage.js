6958bad54f26f1b11eb1eb96ffafae805f4e3248
Angela Wolff <warriorwomenblog@gmail.com>
2018-09-25 16:24:45 +0100

    Making winner and loser arguments clearer

--- backend/models/slackMessage.js
+++ backend/models/slackMessage.js
@@ -4,7 +4,7 @@ function SlackMessage(){
 
 }
 
-SlackMessage.prototype.postMessage = function(player_1, player_2){
+SlackMessage.prototype.postMessage = function(winner, loser){
 
   request.post({
       url: "https://hooks.slack.com/services/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",
@@ -13,10 +13,11 @@ SlackMessage.prototype.postMessage = function(player_1, player_2){
      },
       json: true,
       body: {
-    "text": player_1 + " beats " + player_2 + " into submission"
+    "text": winner + " beats " + loser + " into submission"
     }
   }, function (error, response, body){
       console.log('Response: ', response);
+      
   });
 }
 
