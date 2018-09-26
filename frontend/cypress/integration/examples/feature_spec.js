8e22643b1fa6c120795cb440fcefa44e95214de9
edthomas93 <et1270@my.bristol.ac.uk>
2018-09-26 23:22:03 +0100

    Added feature test for winning game

--- frontend/cypress/integration/examples/feature_spec.js
+++ frontend/cypress/integration/examples/feature_spec.js
@@ -1,23 +1,28 @@
-describe('Log In', () => {
-  it('succesfully performs login action', () => {
-    // visit 'baseUrl'
+describe('Select player', () => {
+  it('choose from list of players', () => {
     cy.visit('/');
-    // assert if we are in good place - search for a 'smarter world phrase
-    cy.contains('smarter world');
-    // search for a div with 'Teachers' caption, and click it
-    cy.get('a[data-testid="main-link-teachers"]').click();
-    // check if url have changed
-    cy.url().should('includes', 'teachers');
-    cy.contains('more time to teach');
-    // get Login button and click it
-    cy.get('button[data-testid="menu-button-login"]').click();
-    // check if url have changed
-    cy.url().should('includes', '/login');
-    // submit inputs and click submit button
-    cy.get('input[data-testid="login-form-username"]').type('test@email.com');
-    cy.get('input[data-testid="login-form-password"]').type('password');
-    cy.get('button[data-testid="login-form-submit"]').click();
-    // verify that we were redirected
-    cy.url({ timeout: 3000 }).should('includes', '/c/');
+    cy.get('#player_selector0').select('Edward Thomas');
+    cy.get('#player_selector1').select('Danielle');
+    cy.get('#start_game_button').click();
+    for (var count=0; count < 21; count++) {
+      cy.get('#plus_id0').click();
+    }
+    cy.get('#end_game').click();
+    cy.contains('Edward Thomas').should('be.visible');
+    cy.contains('WINNER').should('be.visible');
+    cy.contains('Score: 21').should('be.visible');
+
+    // cy.url().should('includes', 'teachers');
+    // cy.contains('more time to teach');
+    // // get Login button and click it
+    // cy.get('button[data-testid="menu-button-login"]').click();
+    // // check if url have changed
+    // cy.url().should('includes', '/login');
+    // // submit inputs and click submit button
+    // cy.get('input[data-testid="login-form-username"]').type('test@email.com');
+    // cy.get('input[data-testid="login-form-password"]').type('password');
+    // cy.get('button[data-testid="login-form-submit"]').click();
+    // // verify that we were redirected
+    // cy.url({ timeout: 3000 }).should('includes', '/c/');
   });
 });
