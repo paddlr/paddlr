5553cd8ca089f8195429905414c7968b77eee67e
dani <daniboo83@gmail.com>
2018-09-26 17:04:49 +0100

    Set initial state

---
+++ frontend/src/redux/reducers/game.reducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+  inProgress: false,
+  player1ID: undefined,
+  player2ID: undefined,
+  player1Score: 0,
+  player2Score: 0,
+  winningPlayer: undefined,
+  serveCount: 0,
+  whichPlayerIsServing: 1,
+};
