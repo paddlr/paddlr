c9c66c2bd24bdc7d7654a5995fd37b6b017a365d
Stephanie <stephanie.o.fashagba@gmail.com>
2018-09-26 16:52:20 +0100

    Added more tests... handing over to Angela

---
+++ backend/test/routes/slackMessageTest.js
@@ -0,0 +1,12 @@
+var SlackMessages =  require('../../models/slackMessage.js');
+var sinon = require('sinon');
+
+describe('Testing slack integration', function(){
+
+  var slackMessage = new SlackMessages();
+
+  it('returns a random string from smackTalk array', function(){
+    sin
+  })
+
+});
