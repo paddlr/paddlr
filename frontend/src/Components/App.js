e6b7314b1924c2ebb6daf029c53edd37fa0e1c4e
Angela Wolff <warriorwomenblog@gmail.com>
2018-09-26 13:17:51 +0100

    Correcting merge conflicts

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -7,8 +7,6 @@ import store from "../redux/store";
 import Routes from "./Routes";
 
 import "../App.css";
-// import Navbar from "./Navbar";
-// import Game from "./Game";
 
 class App extends Component {
   constructor(props) {
@@ -25,40 +23,7 @@ class App extends Component {
         </Router>
       </Provider>
     );
-
-    } else if(this.state.view == 'leaderboard'){ //table should be it's own component, but just as an example
-      return (
-
-      <div>
-     <button onClick ={ () => {this.setState({view: 'game'})}} className = "score_button" >Play a game of ping pong</button>
-
-      this is the leaderboard
-      <table>
-  <tbody>
-    <tr>
-    	<th>Rank</th>
-    	<th>Username</th>
-    	<th>Points</th>
-    </tr>
-    <tr>
-      <td>1</td>
-      <td>Jessie</td>
-      <td>102,345</td>
-	</tr>
-    <tr>
-      <td>2</td>
-      <td>Bob</td>
-      <td>2,321</td>
-	</tr>
-  </tbody>
-</table>
-</div>)
-
-    }
   }
 }
 
 export default App;
-
-// <Navbar />
-// <Game />
