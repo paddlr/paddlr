375d00ae37dc50555857d220a35bd0087382a259
edthomas93 <et1270@my.bristol.ac.uk>
2018-09-27 13:35:38 +0100

    created frame for serve test and did minot formatting

--- frontend/cypress/integration/examples/feature_spec.js
+++ frontend/cypress/integration/examples/feature_spec.js
@@ -1,48 +1,53 @@
 describe('Feature tests', () => {
-  
 
-   describe('scoring scenarios..', () => {
+  describe('scoring scenarios..', () => {
     beforeEach(function(){
       cy.visit('/');
       cy.get('#select_server').select('Edward Thomas');
       cy.get('#select_challenger').select('Danielle');
       cy.get('#start_game_button').click();
     })
-  it('plays a game of 21-0 and correctly displays winner', () => {
-  
-    for (var count=0; count < 21; count++) {
-      cy.get('#plus_idE').click();
-    }
-    cy.get('#end_game').click();
-    cy.contains('Edward Thomas').should('be.visible');
-    cy.contains('Winner!!').should('be.visible');
-    cy.contains('21').should('be.visible');
+    it('plays a game of 21-0 and correctly displays winner', () => {
 
-  });
+      for (var count=0; count < 21; count++) {
+        cy.get('#plus_idE').click();
+      }
+      cy.get('#end_game').click();
+      cy.contains('Edward Thomas').should('be.visible');
+      cy.contains('Winner!!').should('be.visible');
+      cy.contains('21').should('be.visible');
+    });
 
-  it('plays a game of 23-21 and correctly displays winner', () => {
-  
-    for (var count=0; count < 20; count++) {
-      cy.get('#plus_idE').click();
+    describe('plays a game of 23-21', () => {
+      beforeEach(function(){
+        cy.visit('/');
+        cy.get('#select_server').select('Edward Thomas');
+        cy.get('#select_challenger').select('Danielle');
+        cy.get('#start_game_button').click();
+      })
+
+      it('and correctly displays winner', () => {
+
+      for (var count=0; count < 20; count++) {
+        cy.get('#plus_idE').click();
+        cy.get('#plus_idD').click();
+      }
+
       cy.get('#plus_idD').click();
-    
+      cy.get('#plus_idE').click();
+      cy.get('#plus_idE').click();
+      cy.get('#plus_idE').click();
 
-    }
+      cy.get('#end_game').click();
+      cy.contains('Edward Thomas').should('be.visible');
+      cy.contains('Winner!!').should('be.visible');
+      cy.contains('21').should('be.visible');
+    });
 
-    cy.get('#plus_idD').click();
-    cy.get('#plus_idE').click();
-    cy.get('#plus_idE').click();
-    cy.get('#plus_idE').click();
+  it('alternates serves every point at 20-20', () => {
 
-
-    cy.get('#end_game').click();
-    cy.contains('Edward Thomas').should('be.visible');
-    cy.contains('Winner!!').should('be.visible');
-    cy.contains('21').should('be.visible');
-
-  });
-
-})           
+  })
+})
 
 
 });
