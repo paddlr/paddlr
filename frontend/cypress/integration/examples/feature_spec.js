785138630985a40f85f89a76524a15e172dd4cc8
Tim Blanchard <tim.blanchard@hotmail.co.uk>
2018-09-27 13:00:18 +0100

    added next test - 21-23

--- frontend/cypress/integration/examples/feature_spec.js
+++ frontend/cypress/integration/examples/feature_spec.js
@@ -17,7 +17,37 @@ describe('Feature tests', () => {
     cy.contains('Winner!!').should('be.visible');
     cy.contains('21').should('be.visible');
 
-    // cy.url().should('includes', 'teachers');
+  });
+
+  it('plays a game of 23-21 and correctly displays winner', () => {
+  
+    for (var count=0; count < 20; count++) {
+      cy.get('#plus_idE').click();
+      cy.get('#plus_idD').click();
+    
+
+    }
+
+    cy.get('#plus_idD').click();
+    cy.get('#plus_idE').click();
+    cy.get('#plus_idE').click();
+    cy.get('#plus_idE').click();
+
+
+    cy.get('#end_game').click();
+    cy.contains('Edward Thomas').should('be.visible');
+    cy.contains('Winner!!').should('be.visible');
+    cy.contains('21').should('be.visible');
+
+  });
+
+  
+
+
+});
+
+
+  // cy.url().should('includes', 'teachers');
     // cy.contains('more time to teach');
     // // get Login button and click it
     // cy.get('button[data-testid="menu-button-login"]').click();
@@ -29,7 +59,3 @@ describe('Feature tests', () => {
     // cy.get('button[data-testid="login-form-submit"]').click();
     // // verify that we were redirected
     // cy.url({ timeout: 3000 }).should('includes', '/c/');
-  });
-
-
-});
