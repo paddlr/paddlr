4582f4bfcc9909300ab54d2652e7d4964685879f
Tim Blanchard <tim.blanchard@hotmail.co.uk>
2018-09-27 15:13:57 +0100

    4 tests passing

--- frontend/cypress/integration/examples/feature_spec.js
+++ frontend/cypress/integration/examples/feature_spec.js
@@ -1,4 +1,14 @@
 describe("Feature tests", () => {
+  describe("game setup tests", () => {
+    it("two players can be selected", () => {
+      cy.visit("/");
+      cy.get("#select_server").select("Edward Thomas");
+      cy.get("#select_challenger").select("Danielle");
+      cy.get("#start_game_button").click();
+      cy.contains("Edward Thomas").should("be.visible");
+      cy.contains("Danielle").should("be.visible");
+    });
+  });
   describe("scoring scenarios..", () => {
     beforeEach(function() {
       cy.visit("/");
@@ -6,6 +16,22 @@ describe("Feature tests", () => {
       cy.get("#select_challenger").select("Danielle");
       cy.get("#start_game_button").click();
     });
+
+    it("plays a game of 15-14 and displays the correct score ", () => {
+      for (var count = 0; count <14; count++) {
+        cy.get("#plus_idE").click();
+        cy.get("#plus_idD").click();
+ }
+        cy.get("#plus_idE").click();
+        cy.contains("15").should("be.visible");
+        cy.contains("14").should("be.visible");
+
+
+
+
+    });
+
+
     it("plays a game of 21-0 and correctly displays winner", () => {
       for (var count = 0; count < 21; count++) {
         cy.get("#plus_idE").click();
@@ -16,13 +42,10 @@ describe("Feature tests", () => {
       cy.contains("21").should("be.visible");
     });
 
+    
+
     describe("plays a game of 23-21", () => {
-      beforeEach(function() {
-        cy.visit("/");
-        cy.get("#select_server").select("Edward Thomas");
-        cy.get("#select_challenger").select("Danielle");
-        cy.get("#start_game_button").click();
-      });
+     
 
       it("and correctly displays winner", () => {
         for (var count = 0; count < 20; count++) {
